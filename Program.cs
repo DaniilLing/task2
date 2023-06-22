d50a456c2ae8b37f4c97671298c8ca72f4b39203
DaniilLing <136614439+DaniilLing@users.noreply.github.com>
2023-06-22 17:35:29 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("введите первое число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите третье число ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int Mmax = 0;
+if (num1 > num2) 
+{
+   num1 = Mmax;
+}
+else
+{
+    num2 = Mmax;
+}
+if (num2 >num3)
+{
+   num2 = Mmax;
+}
+else
+{
+   num3 = Mmax;
+}
+if (num3 > num1)
+{
+    num3 = Mmax;
+}
+else
+{
+    num1 = Mmax;
+}
+System.Console.WriteLine(Mmax);
